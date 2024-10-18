667fbd161fb57ad96bdf1c894c817f07f89939f9
Clupai8o0 <clupaio4@gmail.com>
2024-10-18 09:50:41 +0545

    product base

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/containers/container";
+import Navbar from "@/components/navigation/navbar";
+
+function Product() {
+	return (
+		<main>
+			<Container parentClassName="bg-[#000]">
+				<Navbar />
+			</Container>
+		</main>
+	);
+}
+
+export default Product;
